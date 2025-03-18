1fca641de4b01a787276fb7089296723f3addd27
Womkel <Podyrio@hotmail.com>
2025-03-18 11:00:39 +0200

    Practice

---
+++ main_5.py
@@ -0,0 +1,8 @@
+def bmi(weight, height):
+    index = weight / (height * height)
+    return index
+patient_5 = bmi(61, 1.83)
+print("underweight", patient_5 < 18.5)
+
+patient_7 = bmi(75, 1.74)
+print("underweight", patient_7 < 18.5)
