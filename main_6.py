17e877288b05ca21425b4fe871888ab83c8b7eef
Womkel <Podyrio@hotmail.com>
2025-03-18 11:18:03 +0200

    Practice

--- main_6.py
+++ main_6.py
@@ -1,6 +1,7 @@
 def rect(leight, width):
     area = leight * width
     perimetr = 2 * leight + 2 * width
-    return area, perimetr
-x, y = rect(50, 100)
-print(x, y)
+    price = 1000 * area
+    return area, perimetr, price
+x, y, e = rect(50, 100)
+print(x, y, e)
